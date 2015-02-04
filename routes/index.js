166b6b47af01528b906825da24def4495581ede0
UBUNTU <ubuntu@ubuntu>
2015-02-04 17:11:19 +0100

    Added gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport')
+    , LocalStrategy = require('passport-local').Strategy;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/',function(req,res)
+{
+   res.render('index');
 });
+
+
+router.post('/login',
+    passport.authenticate('local', { successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true })
+);
 
 module.exports = router;
