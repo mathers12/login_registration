ea37508043ce735ff3fecc0bd93ba0b4133d7e36
Michal Krajnak <michaall.k@gmail.com>
2015-02-04 17:19:56 +0100

    gitingnore

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport')
     , LocalStrategy = require('passport-local').Strategy;
-
+ //fdf
 
 router.get('/',function(req,res)
 {
